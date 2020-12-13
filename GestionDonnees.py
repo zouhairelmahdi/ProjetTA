558a38bf6847c51058aaa83837882bd2206a2c22
younessmouad <mouad.youness@usherbrooke.ca>
2020-12-13 20:06:45 +0100

    regression logistique

--- GestionDonnees.py
+++ GestionDonnees.py
@@ -7,7 +7,8 @@ import matplotlib.pyplot as plt
 import pandas as pd
 from sklearn.preprocessing import LabelEncoder
 from sklearn.preprocessing import StandardScaler
-from sklearn.model_selection import StratifiedKFold
+from sklearn.model_selection import train_test_split
+
 
 class GestionDonnees:
     def __init__(self, distribution_gaussienne, bruit):
@@ -22,30 +23,25 @@ class GestionDonnees:
 
         chemin_entrain = './data/train.csv'
         chemin_test = './data/test.csv'
-        les_donnees_entrain = pd.read_csv(chemin_entrain)
-        les_donnees_test = pd.read_csv(chemin_test)
-
-        id_entrain = les_donnees_entrain.pop('id')
-        classes = les_donnees_entrain.pop('species')
-        id_test = les_donnees_test.pop('id')
+        donnees_entrain = pd.read_csv(chemin_entrain)
+        donnees_nouveau_test = pd.read_csv(chemin_nouveau_test)
         
-        y_entrain = LabelEncoder().fit(classes).transform(classes)
+        donnees_entrain.pop('id')
+        classes = donnees_entrain.pop('species')
+        y = LabelEncoder().fit(classes).transform(classes)
+        
+        id_test = donnees_nouveau_test.pop('id')
         
         if(self.distribution_gaussienne):
-            x_entrain = StandardScaler().fit(les_donnees_entrain).transform(les_donnees_entrain)
-            x_test = StandardScaler().fit(les_donnees_test).transform(les_donnees_test)
+            x = StandardScaler().fit(donnees_entrain).transform(donnees_entrain)
+            x_nouveau_test = StandardScaler().fit(donnees_nouveau_test).transform(donnees_nouveau_test)
         else:
-            x_entrain = les_donnees_entrain.values
-            x_test = les_donnees_test.values
-        
-        return id_entrain, x_entrain, y_entrain, id_test, x_test
-    
-    def generer_donnees_validation_croisee(self, x_entrain, y_entrain):
-        """
-        Generer les donnees pour k=10 validation croisee tel que les 10 sous ensembles contiennent toutes les classes (Stratification)
-        """
-        skf = StratifiedKFold(n_splits=10, shuffle=True) #shuffle: mélanger les données
-
+            x = donnees_entrain.values
+            x_nouveau_test = donnees_nouveau_test.values
+           
+        x_entrain, x_test, y_entrain, y_test = train_test_split(x, y, train_size=0.8, random_state=0, shuffle=True)
+             
+        return X_entrain, y_entrain, x_test, y_test, x_nouveau_test
     
     
     
--- test.py
+++ test.py
@@ -2,7 +2,7 @@ from sklearn.model_selection import GridSearchCV
 from sklearn.model_selection import StratifiedKFold
 from sklearn.metrics import make_scorer, accuracy_score
 
-def classifier_donnees(classifieur, params, x_entrain, y_entrain, x_test):
+def classifier_donnees(classifieur, params, x_entrain, y_entrain, x_test, y_test):
     
     indices_validation_croisee = StratifiedKFold(n_splits=5, shuffle=True)
     justesse_score = make_scorer(accuracy_score)
@@ -13,9 +13,23 @@ def classifier_donnees(classifieur, params, x_entrain, y_entrain, x_test):
                         cv=indices_validation_croisee)
     
     grille_recherche.fit(x_entrain, y_entrain)
-    prediction = grille_recherche.predict(x_test)
-    return prediction
     
+    prediction_entrain = grille_recherche.predict(x_entrain)
+    prediction_test = grille_recherche.predict(x_test)
     
+    justesse_entrain = accuracy_score(y_entrain, prediction_entrain)
+    justesse_test = accuracy_score(y_test, prediction_test)
+
+    meilleure_justesse = grille_recherche.best_score_
+    meilleurs_params = grille_recherche.best_params_
+    meilleur_classifieur = grille_recherche.best_estimator_
     
+    resultat = {}
     
+    resultat['justess_entrain'] = justesse_entrain
+    resultat['justess_test'] = justesse_test
+    resultat['meilleure_justesse'] = meilleure_justesse
+    resultat['meilleurs_params'] = meilleurs_params
+    resultat['meilleur_classifieur'] = meilleur_classifieur
+    
+    return resultat
